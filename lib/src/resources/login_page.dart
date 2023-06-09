c53c5e1ebbcaab49cf2c046eebacf8ddad3313ec
LeTri <trile20021511@gmail.com>
2023-06-09 13:48:58 +0700

    update code

--- lib/src/resources/login_page.dart
+++ lib/src/resources/login_page.dart
@@ -18,9 +18,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  AuthBloc authBloc=AuthBloc();
-  final TextEditingController _passController= TextEditingController();
-  final TextEditingController _emailController= TextEditingController();
+  AuthBloc authBloc = AuthBloc();
+  final TextEditingController _passController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +29,10 @@ class _LoginPageState extends State<LoginPage> {
         child: SafeArea(
           child: Column(
             children: [
-              Image.asset('assets/app-icon1.png', color: Colors.black,),
+              Image.asset(
+                'assets/app-icon1.png',
+                color: Colors.black,
+              ),
               // Container(
               //   width: double.maxFinite,
               //   height: 200,
@@ -41,59 +44,84 @@ class _LoginPageState extends State<LoginPage> {
               //     )
               //   ),
               // ),
-              const SizedBox(height: 20,),
-              const TextLargest(
-                text: "Login to Your Account", 
+              const SizedBox(
+                height: 20,
               ),
-              const SizedBox(height: 6,),
+              const TextLargest(
+                text: "Login to Your Account",
+              ),
+              const SizedBox(
+                height: 6,
+              ),
               // const TextSmall(
               //   text: 'Login to continue using iCab',
-              //   color: Colors.black54, 
+              //   color: Colors.black54,
               // ),
-              const SizedBox(height: 60,),
-              TextFieldPage(
-                stream: authBloc.emailStream,
-                controller: _emailController,
-                text: 'Email',
-                icon: const Icon(Icons.mail_outline, color: Colors.black26,)
+              const SizedBox(
+                height: 60,
               ),
               TextFieldPage(
-                stream: authBloc.passStream,
-                controller: _passController,
-                text: 'Password',
-                obscureText: true,
-                icon: const Icon(Icons.lock_outline_rounded, color: Colors.black26,)
-              ),
-               Row(
+                  stream: authBloc.emailStream,
+                  controller: _emailController,
+                  text: 'Email',
+                  icon: const Icon(
+                    Icons.mail_outline,
+                    color: Colors.black26,
+                  )),
+              TextFieldPage(
+                  stream: authBloc.passStream,
+                  controller: _passController,
+                  text: 'Password',
+                  obscureText: true,
+                  icon: const Icon(
+                    Icons.lock_outline_rounded,
+                    color: Colors.black26,
+                  )),
+              Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                 children: const [
-                   CheckBox(),
-                   SizedBox(width: 20),
-                   TextSmall(text: 'Forgot password?', color: Colors.black38,),
-                 ],
-               ),
-        
-              const SizedBox(height: 40,),
-              
+                children: const [
+                  CheckBox(),
+                  SizedBox(width: 20),
+                  TextSmall(
+                    text: 'Forgot password?',
+                    color: Colors.black38,
+                  ),
+                ],
+              ),
+
+              const SizedBox(
+                height: 40,
+              ),
+
               ButtonPage(
                 onTap: _onLoginClick,
-                text: 'Sign In', 
-                color: Colors.black, 
+                text: 'Sign In',
+                color: Colors.black,
                 colortext: Colors.white,
                 width: double.maxFinite,
                 height: 50,
               ),
-        
+
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const TextSmall(text: 'Dont have an account? ', color: Colors.black45,),
+                  const TextSmall(
+                    text: 'Dont have an account? ',
+                    color: Colors.black45,
+                  ),
                   InkWell(
-                    onTap: (){
-                      Navigator.push(context, MaterialPageRoute(
-                        builder: (context) => const RegisterPage(),));
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const RegisterPage(),
+                          ));
                     },
-                    child: const TextSmall(text: 'Sign up', color: Colors.black,))
+                    child: const TextSmall(
+                      text: 'Sign up',
+                      color: Colors.black,
+                    ),
+                  )
                 ],
               )
             ],
@@ -103,20 +131,16 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  void _onLoginClick(){
+  void _onLoginClick() {
     LoadingDialog.showLoadingDialog(context, 'loading...');
-    authBloc.signIn(
-      _emailController.text.trim(),
-      _passController.text.trim(),
-      (){
-        LoadingDialog.hideLoadingDialog(context);
-        Navigator.push(context, MaterialPageRoute(
-          builder: (context)=> const HomePage()));
-      },
-      (msg){
-        LoadingDialog.hideLoadingDialog(context);
-        MsgDialog.showMsgDialog(context, 'Error', msg);
-      }
-    );
+    authBloc.signIn(_emailController.text.trim(), _passController.text.trim(),
+        () {
+      LoadingDialog.hideLoadingDialog(context);
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => const HomePage()));
+    }, (msg) {
+      LoadingDialog.hideLoadingDialog(context);
+      MsgDialog.showMsgDialog(context, 'Error', msg);
+    });
   }
 }
